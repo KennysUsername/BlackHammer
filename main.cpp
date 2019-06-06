33327a89c213be8866ac8633c338f3eb97b14393
Kenny Bui <MinionTrooper69@users.noreply.github.com>
2019-06-05 23:15:29 -0400

    Inital Release
    Start of my code for Project Black Hammer

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+    //Index
+    void LootRandomizer();
+
+
+int main()
+{
+
+    //Menu
+    cout << "What to do?" << endl;
+    cout << "(enter number for input)" << endl;
+    cout << "(1) Loot Randomizer" << endl;
+    cout << "(2)" << endl;
+    cout << "(3)" << endl;
+
+
+    int Input1 = 0;
+    cin >> Input1;
+    switch (Input1)
+    {
+        case 1:
+        LootRandomizer();
+        break;
+
+        case 2:
+        cout << "What to 2do?" << endl;
+        break;
+
+        case 3:
+        cout << "What to 3do?" << endl;
+        break;
+    }
+
+}
+
+    void LootRandomizer()
+    {
+        cout << "What to do?" << endl;
+    }
+
+
+
+
