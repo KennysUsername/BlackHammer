c3644762f42b948707d177f2689354475dfb4ffa
Kenny Bui <MinionTrooper69@users.noreply.github.com>
2019-06-06 11:44:09 -0400

    Rand() Thing now works, Yay
    Ok so i finally got random to work

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -43,24 +45,56 @@ int main()
         //LootValue is used for how many times the randomizer occurs
         //Decider should add a random string from Rarity with Type to output the Answer
 
+        //Test
+        //cout << Rarity[1] + Type[1] << endl;
+
+
+        //random number generator
+
+
         string Rarity[] = {"Common ", "Uncommon ", "Rare ", "Legendary ", "Mystic "};
         string Type[] = {"Weapon", "Armor", "Item"};
+
+
 
         cout << " " << endl;
         cout << "Input loot amount" << endl;
 
         //Input Repeat amount
         int LootValue;
-        cin >> LootValue
+        cin >> LootValue;
 
-        int i = 0;
-        while (i < LootValue + 1)
+        //randomizer, holds p1 as Rarity and p2 as Item Type
+        srand(time(0));
+        int Percentage = rand() % 100 + 1;
+        string p1;
+        string p2;
+
+        //common (60% - 100%) (40%)
+        if (Percentage > 60)
         {
-
-        cout << Rarity[rand() % Rarity.length()] + Type[Rand() % Type.length()]
-        i++
-
+        p1 = Rarity[0];
         }
+
+        //Uncommon (20% - 59%) (39%)
+        if (Percentage > 61)
+        {
+        p1 = Rarity[1];
+        }
+
+        //Rare (3% - 19%) (16%)
+        if (Percentage > 84)
+        {
+        p1 = Rarity[2];
+        }
+
+        //Legendary (2% - 0%) (2%)
+        if (Percentage > 98)
+        {
+        p1 = Rarity[3];
+        }
+
+        cout << p1 << endl;
 
     }
 
